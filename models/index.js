3e4bdbb806c2d29f42d143facb574505468fbb98
Hannah <94648556+hannahj25@users.noreply.github.com>
2022-03-17 18:23:04 +1030

    wrote associations methods for sequelize models in models/index.js

--- models/index.js
+++ models/index.js
@@ -5,12 +5,31 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.BelongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    foreignKey: 'product_id',
+  },
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.BelongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    foreignKey: 'tag_id',
+  }
+});
 
 module.exports = {
   Product,
